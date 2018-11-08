367419b4f4522ce154ff511e5a9cb92300e03ccc
Diogo Cardoso <diogocardoso92@outlook.com>
2018-11-08 00:58:24 +0000

    Fix validation function and add new

--- server/src/middleware/validation.ts
+++ server/src/middleware/validation.ts
@@ -1,31 +1,40 @@
 import Joi from 'joi';
 import _ from 'lodash';
-import { Request, Response, NextFunction} from 'express';
+import { Request, Response, NextFunction } from 'express';
 
-export function validateNotification(req: Request, res: Response, next: NextFunction) {
+export function validateCreateNotification(req: Request, res: Response, next: NextFunction) {
   const { email } = req.body;
   const minPrize = Number.parseInt(req.body.minprize, 10);
-
-  if (!email || !_.isString(email)) {
-    res.status(400).json({ error: 'Please insert a valid email. '});
-  }
-  if (!minPrize || !_.isFinite(minPrize)) {
-    res.status(400).json({ error: 'Please insert a valid prize value. '});
-  }
 
   const notificationObj = { email, minPrize };
 
   const schema = {
     email: Joi.string().email().required(),
     minPrize: Joi.number().integer().min(15).max(300).required(),
-  }
+  };
+
   const validationResult = Joi.validate(notificationObj, schema);
+
   if (validationResult.error) {
     console.log('validation: ', validationResult.error);
-    res.status(400).json({ error: validationResult.error});
+    res.status(400).json({ error: validationResult.error });
   } else {
     // Send the notification object to the next middleware
     res.locals.notificationObj = notificationObj;
+    next();
+  }
+};
+
+export function validateEmail(req: Request, res: Response, next: NextFunction) {
+  const { email } = req.body;
+  const schema = Joi.string().email().required();
+  const validationResult = Joi.validate({ email }, schema);
+  if (validationResult.error) {
+    console.log('validation: ', validationResult.error);
+    res.status(400).json({ error: validationResult.error });
+  } else {
+    // Send the notification object to the next middleware
+    res.locals.email = email;
     next();
   }
 };
