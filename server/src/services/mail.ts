6bfb66e60afbdf571f971455d5869b60e905d9d1
Diogo Cardoso <diogocardoso92@outlook.com>
2018-11-17 22:42:22 +0000

    Add edit and remove emails

--- server/src/services/mail.ts
+++ server/src/services/mail.ts
@@ -22,3 +22,33 @@ export function sendWelcomeEmail(to: string, variables: any) {
   };
   return sgMail.send(msg);
 }
+
+export function sendEditEmail(to: string, variables: any) {
+  if (!isProd) { return Promise.resolve(); }
+  const msg = {
+    to,
+    from: EMAIL_ADDRESS,
+    templateId: 'd-5d540038d0974581968265d357e71b4d',
+    dynamic_template_data: {
+      minPrizeUpdated: variables.minprize,
+      updateUrl: URL.UPDATE({ token: variables.token }),
+      indexUrl: URL.INDEX,
+    }
+  };
+  return sgMail.send(msg);
+}
+
+export function sendDeleteEmail(to: string, variables: any) {
+  if (!isProd) { return Promise.resolve(); }
+  const msg = {
+    to,
+    from: EMAIL_ADDRESS,
+    templateId: 'd-fc48fa042b3e40168b8fb0bf9de7aeb0',
+    dynamic_template_data: {
+      deleteUrl: URL.DELETE({ token: variables.token }),
+      indexUrl: URL.INDEX,
+    }
+  };
+  return sgMail.send(msg);
+}
+
