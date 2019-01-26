60cd264e09a7e0d96ea4b5385d66ac2432b0ad06
Diogo Cardoso <diogocardoso92@outlook.com>
2019-01-26 01:17:02 +0000

    Add title component

---
+++ client/src/components/Title/Title.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Title.css';
+
+export default function Title() {
+  return (
+    <div className="title">
+      <h1> Euromillions Notify </h1>
+      <h3> Don't miss the change of winning! </h3>
+    </div>
+  )
+}
--- client/src/containers/App/App.tsx
+++ client/src/containers/App/App.tsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Title from '../../components/Title/Title';
+
 class App extends Component {
   render() {
     return (
-      <p>hello</p>
+      <Title />
     );
   }
 }
