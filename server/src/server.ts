0029dfdfde49fd3aeee5e75ff52b92ad4f546345
Diogo Cardoso <diogocardoso92@outlook.com>
2019-03-19 23:43:49 +0000

    Add env var validation on startup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,48 @@
 import dotenv from "dotenv";
-
+import _ from "lodash";
 import app from "./app";
 import logger from "./utils/logger";
 
-dotenv.config({ path: ".env" });
-const { PORT, NODE_ENV } = process.env;
+dotenv.config();
+
+const {
+  PORT,
+  NODE_ENV,
+  MONGODB_URI,
+  EMAIL_SENDGRID_API_KEY,
+  EMAIL_ADDRESS
+} = process.env;
+const PORT_N = Number.parseInt(PORT, 10);
+const isProd = NODE_ENV === "production";
+
+if (!_.isFinite(PORT_N)) {
+  logger.error("You need to set a PORT in the .env file");
+  process.exit(-1);
+}
+
+if (_.isNil(NODE_ENV) || !_.isString(NODE_ENV)) {
+  logger.error("You need to set a NODE_ENV in the .env file");
+  process.exit(-1);
+}
+
+if (!_.isString(MONGODB_URI)) {
+  logger.error("You need to set a MONGODB_URI in the .env file");
+  process.exit(-1);
+}
+
+if (isProd) {
+  if (!_.isString(EMAIL_SENDGRID_API_KEY)) {
+    logger.error("You need to set a MONGODB_URI in the .env file");
+    process.exit(-1);
+  }
+
+  if (!_.isString(EMAIL_ADDRESS)) {
+    logger.error("You need to set a MONGODB_URI in the .env file");
+    process.exit(-1);
+  }
+}
 
 // Start Express
-app.listen(PORT, () => {
-  logger.info(`App listening on port ${PORT} in ${NODE_ENV} mode`);
+app.listen(PORT_N, () => {
+  logger.info(`Started on port ${PORT} in ${NODE_ENV} mode`);
 });
